7df3405705ea10a52bd4a51fcb5e133117321c30
Alison <allylavanda@aol.com>
2022-05-02 22:01:25 -0400

    Update PlayerDataManager.java

--- src/main/java/me/sjaeledyr/hooftoheartplus/util/PlayerDataManager.java
+++ src/main/java/me/sjaeledyr/hooftoheartplus/util/PlayerDataManager.java
@@ -8,17 +8,19 @@ public class PlayerDataManager {
     private Main main = Main.getPlugin(Main.class);
     FileConfiguration cfg = main.getConfig();
 
+    // Document that a player has joined the server before
     public void addPlayerCheck(Player p, boolean isFirstJoin) {
         boolean i = cfg.getBoolean(p.getName() + "." + "isFirstJoin");
         boolean a = isFirstJoin;
         cfg.set(p.getName() + "." + "isFirstJoin", false);
         main.saveConfig();
     }
-
+    // See if it is a player's first time joining the server
     public boolean newPlayerCheck(Player p) {
         return cfg.getBoolean(p.getName() + "." + "isFirstJoin");
     }
 
+    // Document player IP address
     public void listIp(Player p, String ip) {
         cfg.set(p.getName() + "." + "lastIpAddress", ip);
     }
