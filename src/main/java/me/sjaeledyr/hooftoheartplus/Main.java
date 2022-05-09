81198f56548a1ebc2bcaa70dc3e2aed549940341
Alison <allylavanda@aol.com>
2022-05-09 19:33:36 -0400

    Update Main.java

--- src/main/java/me/sjaeledyr/hooftoheartplus/Main.java
+++ src/main/java/me/sjaeledyr/hooftoheartplus/Main.java
@@ -1,21 +1,17 @@
 package me.sjaeledyr.hooftoheartplus;
 
 import me.sjaeledyr.hooftoheartplus.util.EventListener;
-import org.bukkit.configuration.file.FileConfiguration;
+import me.sjaeledyr.hooftoheartplus.util.PlayerDataManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
-    public static FileConfiguration config;
+    public PlayerDataManager playerData;
 
     @Override
     public void onEnable() {
+        this.playerData = new PlayerDataManager(this);
         // Plugin startup logic
         System.out.println("[HoofToHeartPlus] HoofToHeartPlus by Sjaeledyr has been loaded!");
-
-        // Init Config
-        config.set("player_data", true);
-        config = getConfig();
-        this.saveDefaultConfig();
 
         // Init Event Listener
         getServer().getPluginManager().registerEvents(new EventListener(), this);
